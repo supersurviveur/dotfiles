42fa083ad00e5e9d19ab12a1b91c7f8c77f56fb5
supersurviveur <julien.thillard38@gmail.com>
2024-04-26 13:20:58 +0200

    Add an installation script

---
+++ install.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+ask() {
+    while true; do
+        read -p "$1 [Y/n] " yn
+        case $yn in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            "" ) return 0;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+askN() {
+    while true; do
+        read -p "$1 [y/N] " yn
+        case $yn in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            "" ) return 1;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+# Global config
+cp .config/sway ~/.config -r
+cp .config/zathura ~/.config -r
+cp .config/alacritty.toml ~/.config
+cp script/{augment_lum.sh,dim_lum.sh,init-sway,exit-sway,init} ~/script
+cp .p10k.zsh ~/
+cp .zprofile ~/
+cp .zshrc ~/
+cp .zshenv ~/
+
+# Per computer config
+cp .config/waybar ~/.config -r
+if askN "Add a custom temperature?"; then
+    sed -i '/"temperature": {/a \\t\t"TODO": "TODO",' ~/.config/waybar/config
+fi
+
+if ask "Use gammastep?"; then
+    cp .config/gammastep ~/.config -r
+else
+    sed -i '/gammastep/d' ~/script/init
+    sed -i '/gammastep/d' ~/script/exit-sway
+fi
+
+cd services
+if ask "Use asus-numpad?"; then
+    SERVICES_ARGS=asus-numpad
+fi
+./install-services.sh $SERVICES_ARGS
+cd ..
+
+
+if ask "Reboot now?"; then
+    sudo reboot
+fi
--- services/install-services.sh
+++ services/install-services.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 # Install/update custom services files
-cp {custom-asus-numpad,custom-rclone} /etc/init.d/
 
-chmod +x /etc/init.d/{custom-asus-numpad,custom-rclone}
+# We need permissions
+if [ "$UID" != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
 
-rc-update add custom-asus-numpad
+cp custom-rclone /etc/init.d/
+chmod +x /etc/init.d/custom-rclone
 rc-update add custom-rclone
+
+# Install asus-numpad only if asus-numpad is passed in first-parameter
+if [ "$1" = "asus-numpad" ]; then
+    cp custom-asus-numpad /etc/init.d/
+    chmod +x /etc/init.d/custom-asus-numpad
+    rc-update add custom-asus-numpad
+fi
