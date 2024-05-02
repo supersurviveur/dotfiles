bb5e3ad554988f3655cdc84502faa0627078626b
supersurviveur <julien.thillard38@gmail.com>
2024-05-02 10:13:45 +0200

    add rfkill and switch keyboard

--- services/install-services.sh
+++ services/install-services.sh
@@ -11,6 +11,10 @@ cp custom-rclone /etc/init.d/
 chmod +x /etc/init.d/custom-rclone
 rc-update add custom-rclone
 
+cp custom-rfkill /etc/init.d/
+chmod +x /etc/init.d/custom-rfkill
+rc-update add custom-rfkill
+
 # Install asus-numpad only if asus-numpad is passed in first-parameter
 if [ "$1" = "asus-numpad" ]; then
     cp custom-asus-numpad /etc/init.d/
